ff948f318544403609fc5184f3c4849bc80b425a
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-04-19 00:05:37 +0200

    Todo array

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import Todo from './components/ToDo'
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with husband',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Todo/>
       </div>
     );
   }
