241e78faccf070278e571ac71018dd7b637ba06d
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-04-19 01:56:29 +0200

    ToDoITem init2

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,2 +1,14 @@
 import React, { Component } from 'react';
 
+export class ToDoItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+
+        )
+    }
+}
+
+export default ToDoItem
